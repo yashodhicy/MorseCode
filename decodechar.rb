62d73970575e8e7525b920b9ddd2b6bcb578a898
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-07-19 17:42:41 +0300

    Add hash for morse code and decode char method

---
+++ decodechar.rb
@@ -0,0 +1,20 @@
+class DecodeMorseCode
+    MORSE_CODE = Hash[
+  '.-'    => 'A',   '-...'  => 'B',   '-.-.'  => 'C',   '-..'   => 'D',
+  '.'     => 'E',   '..-.'  => 'F',   '--.'   => 'G',   '....'  => 'H',
+  '..'    => 'I',   '.---'  => 'J',   '-.-'   => 'K',   '.-..'  => 'L',
+  '--'    => 'M',   '-.'    => 'N',   '---'   => 'O',   '.--.'  => 'P',
+  '--.-'  => 'Q',   '.-.'   => 'R',   '...'   => 'S',   '-'     => 'T',
+  '..-'   => 'U',   '...-'  => 'V',   '.--'   => 'W',   '-..-'  => 'X',
+  '-.--'  => 'Y',   '--..'  => 'Z'
+  ]
+
+  def decode_char(char)
+    MORSE_CODE[char] || ''
+  end
+
+  
+end
+
+decoder = DecodeMorseCode.new
+puts decoder.decode_char('..')
